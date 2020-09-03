75d55e998ab486a9a0d242a7ee9d756c1978adc0
vcslav-v <vcslav.v@gmail.com>
2020-09-03 18:26:45 +0300

    refactoring calc game

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -5,25 +5,6 @@ from random import choice, randint
 from brain_games import config
 
 
-def solver(num1: int, num2: int, operation: str) -> str:
-    """Solve expression like num1 {operation} num2.
-
-    Parameters:
-        num1: First number in expression
-        num2: Second number in expression
-        operation: May be '+', '-' or '*'
-
-    Returns:
-        Result of the expression
-    """
-    if operation == config.PLUS:
-        return str(num1 + num2)
-    elif operation == config.MINUS:
-        return str(num1 - num2)
-    elif operation == config.MULTIPLY:
-        return str(num1 * num2)
-
-
 def questions_generator() -> tuple:
     """Generate quest/answer for Calculator game.
 
@@ -38,11 +19,11 @@ def questions_generator() -> tuple:
         config.MIN_NUMBER_FOR_CALCULATOR_GAME,
         config.MAX_NUMBER_FOR_CALCULATOR_GAME,
         )
-    operation = choice(config.OPERATIONS)  # noqa: S311
+    operation = choice(list(config.OPERATIONS))  # noqa: S311
     expression = config.QUEST_EVAL_STRING.format(
         num1=str(num1),
         operation=operation,
         num2=str(num2),
         )
-    right_answer = solver(num1, num2, operation)
+    right_answer = str(config.OPERATIONS[operation](num1, num2))
     return (expression, right_answer)
